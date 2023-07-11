db2f371c0ff0a5774f936163cb2f22bc7b28d024
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-07-11 21:19:56 +0800

    update

---
+++ src/components/counterForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function CounterForm() {
+  const [count, setCount] = useState(0);
+  const [components, setComponents] = useState([]);
+
+  const handleAddComponent = () => {
+    setCount(count + 1);
+    setComponents([...components, <div key={count}>Component #{count + 1} <button onClick={() => handleDeleteComponent(count)}>Delete</button></div>]);
+  };
+
+  const handleDeleteComponent = (index) => {
+    setCount(count - 1);
+    setComponents(components.filter((_, i) => i !== index));
+  };
+
+  return (
+    <form>
+      <label>
+        Current count: {count}
+      </label>
+      <br />
+      <button type="button" onClick={handleAddComponent}>
+        Add Component
+      </button>
+      <br />
+      {components}
+    </form>
+  );
+}
+
