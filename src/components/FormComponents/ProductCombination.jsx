73dff03439a24964a0576b3969e5487ad1462abe
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-10-15 00:17:57 +0800

    can display combination

--- src/components/FormComponents/ProductCombination.jsx
+++ src/components/FormComponents/ProductCombination.jsx
@@ -20,17 +20,38 @@ function ProductCombination({headings,rowValues=[],update}) {
     
     function Variants({data}) {
         const nOfType = data.length
-        var productNameList = []
-        return (
-            data[0].option.map((v,i)=>{
-                for (let index = 1; index < nOfType; index++) {
-                    data[i].option.forEach((o,index)=>{
-                        productNameList.push(v+)
-                    })
-                    
-                }
+        var variantOptions = []
+        for (let index = 0; index < data.length; index++) {
+            variantOptions.push(data[index].option.map(text=>{
+                return text
+            }))
+        }
+        
+        function findCombinations(arrays) {
+        function backtrack(currCombination, remainingArrays) {
+            if (remainingArrays.length === 0) {
+            combinations.push(currCombination.join(' '));//join('_')
+            return;
+            }
+        
+            const currentArray = remainingArrays[0];
+            for (let i = 0; i < currentArray.length; i++) {
+            const element = currentArray[i];
+            backtrack(currCombination.concat(element), remainingArrays.slice(1));
+            }
+        }
+        
+        const combinations = [];
+        backtrack([], arrays);
+        return combinations;
+        }
+        variantOptions = findCombinations(variantOptions)
+        return(
+            variantOptions.map(options=>{
+                return <div className="">{options}</div>
             })
         )
+        
     }
     
   
@@ -85,68 +106,71 @@ function ProductCombination({headings,rowValues=[],update}) {
     }
     
     return (
-        <div className="w-full grid grid-flow-row grid-cols-3 gap-2 text-sm border rounded-lg">
-            <div className="">
-                <div className="">Input</div>
-                <input 
-                    type="text" 
-                    placeholder='e.g size, color'
-                    value={optionInput}
-                    onChange={(e)=>{
-                        console.log("input",e.target.value)
-                        setoptionInput(e.target.value)
-                    }}
-                    onKeyDown={(e)=>{ 
-                        if(e.key=== 'Enter'){
-                            console.log("added option")
-                            Data.push({text:optionInput,option:[]})
-                            console.log("Data",Data)
-                            setData([...Data])
-                            setoptionInput("")
-                        }
-                        
-                    }}
-                />
+        <div className="flex flex-col">
+            <div className="w-full grid grid-flow-row grid-cols-3 gap-2 text-sm border rounded-lg">
                 <div className="">
-                    <div className="">Combine</div>
-                    <List sortable onSort={handleSortEnd}>
-                        {Data.map(({ text, disabled }, index) => (
-                            <List.Item 
-                                key={index} 
-                                index={index}
-                                className='border rounded-lg'
-                                size='xs'
-                            >
-                                <div className="w-full flex flex-row justify-between">
-                                    <div className="">
-                                        {text}
+                    <div className="">Input</div>
+                    <input 
+                        type="text" 
+                        placeholder='e.g size, color'
+                        value={optionInput}
+                        onChange={(e)=>{
+                            console.log("input",e.target.value)
+                            setoptionInput(e.target.value)
+                        }}
+                        onKeyDown={(e)=>{ 
+                            if(e.key=== 'Enter'){
+                                console.log("added option")
+                                Data.push({text:optionInput,option:[]})
+                                console.log("Data",Data)
+                                setData([...Data])
+                                setoptionInput("")
+                            }
+                            
+                        }}
+                    />
+                    <div className="">
+                        <div className="">Combine</div>
+                        <List sortable onSort={handleSortEnd}>
+                            {Data.map(({ text, disabled }, index) => (
+                                <List.Item 
+                                    key={index} 
+                                    index={index}
+                                    className='border rounded-lg'
+                                    size='xs'
+                                >
+                                    <div className="w-full flex flex-row justify-between">
+                                        <div className="">
+                                            {text}
+                                        </div>
+                                        <button
+                                            onClick={()=>{
+                                                Data.splice(index,1);
+                                                setData([...Data])
+                                            }}
+                                            className='text-red-600'
+                                        >
+                                            x
+                                        </button>
                                     </div>
-                                    <button
-                                        onClick={()=>{
-                                            Data.splice(index,1);
-                                            setData([...Data])
-                                        }}
-                                        className='text-red-600'
-                                    >
-                                        x
-                                    </button>
-                                </div>
-                            </List.Item>
-                        ))}
-                    </List>
+                                </List.Item>
+                            ))}
+                        </List>
+                    </div>
                 </div>
-            </div>
-            
-            <div className="">
-                <div className="">Options</div>
-                {
-                    Data.map((option,index)=>{ //{ text, disabled } => key in object
-                        console.log("passed option",option)
-                        return(
-                            <OptionInput option={option} index={index}/>
-                        )
-                    })
-                }
+                
+                <div className="">
+                    <div className="">Options</div>
+                    {
+                        Data.map((option,index)=>{ //{ text, disabled } => key in object
+                            console.log("passed option",option)
+                            return(
+                                <OptionInput option={option} index={index}/>
+                            )
+                        })
+                    }
+                    
+                </div>
                 
             </div>
             <div className="">
@@ -157,6 +181,7 @@ function ProductCombination({headings,rowValues=[],update}) {
                 
             </div>
         </div>
+        
     )
 }
 
