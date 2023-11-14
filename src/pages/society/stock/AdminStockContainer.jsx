18aa0f1fe5f4d9064914871846c9e6845d4501d8
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-11-14 16:57:44 +0800

    test

--- src/pages/society/stock/AdminStockContainer.jsx
+++ src/pages/society/stock/AdminStockContainer.jsx
@@ -1,12 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { postURL } from '../../../utils/fetch'
+import { toast } from 'sonner'
 
 function AdminStockContainer({code}) {
+    const [Stocks, setStocks] = useState()
     useEffect(() => {
-        postURL('/api/findsocietystock',true,{code:code})
+        postURL('/api/findsocietystock',true,{code:code}).then(result=>{
+            if(result.success){
+                setStocks(result.data)
+                toast.success("loaded stocks")
+            }else{
+                toast.error("failed to get stocks")
+            }
+            
+        })
     }, [])
     return (
-        <div>AdminStockContainer</div>
+        <div>
+            <div className="text-blue">AdminStockContainer</div>
+            {
+                Stocks&&(
+                    
+                        Stocks.map(s=>{
+                            return(
+                                <div className="">{s._id}</div>
+                            )
+                        })
+                    
+                )
+            }
+        </div>
         
         
     )
